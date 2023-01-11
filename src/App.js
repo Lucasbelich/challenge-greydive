5b646f566379e7bf39f7ec60e3230356e08819ac
Lucasbelich <lucasbelich1@gmail.com>
2023-01-10 23:40:20 -0300

    fix basename

--- src/App.js
+++ src/App.js
@@ -3,18 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Form from "./components/Form/Form";
 import Registered from "./components/Registered/Registered";
 
-
-
 function App() {
   return (
-    <div className="App">
-      
-        <Routes>
-          <Route path="*" element={<h1>NOT FOUND 404</h1>} />
-          <Route path="/" element={<Form />} />
-          <Route path="/registered" element={<Registered />} />
-        </Routes>
-      
+    <div>
+      <Routes>
+        <Route path="/" element={<Form />} />
+        <Route path="/registered" element={<Registered />} />
+      </Routes>
     </div>
   );
 }
